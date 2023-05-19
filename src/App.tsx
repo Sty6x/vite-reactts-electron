a7152b5b17cc28e81e9834c83f00940069bde95b
francis <franzdiaz460@gmail.com>
2023-05-19 22:12:53 +0400

    ref: codemirror6 is confusing

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,26 @@ import logoVite from "./assets/logo-vite.svg";
 import logoElectron from "./assets/logo-electron.svg";
 import "./App.scss";
 import ReactMarkdown from "react-markdown";
-
-console.log(
-	"[App.tsx]",
-	`Hello world from Electron ${process.versions.electron}!`
-);
-
 interface InputProps {
 	handleChange: (e: React.ChangeEvent<HTMLInputElement>) => null;
 }
 function App() {
 	const [count, setCount] = useState(0);
 	const [userInput, setUserInput] = useState<string>("");
-
+	const editorRef = useRef<HTMLDivElement>();
 	function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
 		const inputValue: string = e.target.value;
 		setUserInput(inputValue);
 	}
-
 	return (
 		<div className="App">
 			<div>
 				<h1>Editor</h1>
-				<div>
-					<textarea
-						style={{ color: "black" }}
-						value={userInput}
-						onChange={handleChange}
-					/>
-				</div>
+				<div ref={editorRef} id="editor-Parent"></div>
 			</div>
 			<div>
 				<h1>Markdown</h1>
-				<ReactMarkdown children={userInput} />
+				<ReactMarkdown>{userInput}</ReactMarkdown>
 			</div>
 		</div>
 	);
