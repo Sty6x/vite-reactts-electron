be77a282d35ed81579639e515ce88f74a8971fbb
francis <franzdiaz460@gmail.com>
2023-05-22 02:19:48 +0400

    had to remove styles to code mirror from my css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useRef, useState } from "react";
-import logoVite from "./assets/logo-vite.svg";
-import logoElectron from "./assets/logo-electron.svg";
+import React, { useRef, useState, useCallback } from "react";
 import "./App.scss";
 import ReactMarkdown from "react-markdown";
+import CodeMirror from "@uiw/react-codemirror";
 interface InputProps {
 	handleChange: (e: React.ChangeEvent<HTMLInputElement>) => null;
 }
+interface CallbackProps {}
 function App() {
 	const [count, setCount] = useState(0);
 	const [userInput, setUserInput] = useState<string>("");
 	const editorRef = useRef<HTMLDivElement>();
-
+	const onChange = React.useCallback((value: string, viewUpdate) => {
+		console.log("value:", value);
+	}, []);
 	function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
 		const inputValue: string = e.target.value;
 		setUserInput(inputValue);
@@ -18,12 +20,15 @@ function App() {
 	return (
 		<div className="App">
 			<div>
-				<div ref={editorRef} id="editor-parent" />
-			</div>
-			<div>
 				<h1>Markdown</h1>
 				<ReactMarkdown>{userInput}</ReactMarkdown>
 			</div>
+			<CodeMirror
+				value="random text"
+				onChange={onChange}
+				width="100%"
+				height="100%"
+			/>
 		</div>
 	);
 }
