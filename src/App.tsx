10e114219491b6111f9448c27bc0d1ff6a69bf03
francis <franzdiaz460@gmail.com>
2023-05-22 05:24:01 +0400

    vim keymaps work out of the box

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useRef, useState, useCallback } from "react";
 import "./App.scss";
 import ReactMarkdown from "react-markdown";
-import CodeMirror from "@uiw/react-codemirror";
+import CodeMirror, { getStatistics } from "@uiw/react-codemirror";
+import { vim } from "@replit/codemirror-vim";
 interface InputProps {
 	handleChange: (e: React.ChangeEvent<HTMLInputElement>) => null;
 }
@@ -10,25 +11,30 @@ function App() {
 	const [count, setCount] = useState(0);
 	const [userInput, setUserInput] = useState<string>("");
 	const editorRef = useRef<HTMLDivElement>();
-	const onChange = React.useCallback((value: string, viewUpdate) => {
+	const onChange = useCallback((value: string, viewUpdate) => {
 		console.log("value:", value);
 	}, []);
-	function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+	function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
 		const inputValue: string = e.target.value;
 		setUserInput(inputValue);
 	}
 	return (
 		<div className="App">
-			<div>
-				<h1>Markdown</h1>
-				<ReactMarkdown>{userInput}</ReactMarkdown>
+			<div className="list">
+				{["a", "b", "c", "d"].map((num) => {
+					return <li>{num}</li>;
+				})}
 			</div>
-			<CodeMirror
-				value="random text"
-				onChange={onChange}
-				width="100%"
-				height="100%"
-			/>
+			<div className="editor-container">
+				<CodeMirror
+					value="random text"
+					onChange={onChange}
+					maxWidth="100%"
+					minHeight="100vh"
+					extensions={[vim()]}
+					basicSetup={{ lineNumbers: true, highlightActiveLine: false }}
+				/>
+			</div>
 		</div>
 	);
 }
