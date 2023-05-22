dd3af0634562b49e2e9e501c2d376e2a35b1a3ca
francis <franzdiaz460@gmail.com>
2023-05-23 00:08:57 +0400

    making editor resizeable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback } from "react";
+import React, { useRef, useState, useCallback, useEffect } from "react";
 import "./App.scss";
 import CodeMirror, { getStatistics } from "@uiw/react-codemirror";
 import { vim } from "@replit/codemirror-vim";
@@ -18,13 +18,21 @@ function App() {
 	const [userInput, setUserInput] = useState<string>(`
 # Header 1
 ## jsCode snippet and some shit that i dont understand
+		This is a code snippet
+>Line break  
+>Another Line Break
+
+### This is a list
+1. Ordered list item 1
+2. Ordered list item 2
+3. Ordered list item 3
 `);
 	const [editorLines, setEditorLines] = useState<number>();
 	const editorRef = useRef<HTMLTextAreaElement>();
 	const onChange = useCallback((value: string, viewUpdate) => {
 		setUserInput(value);
 	}, []);
-	const [resizeWidth, setResizeWidth] = useState<string>();
+	const [resizeWidth, setResizeWidth] = useState<number>(50);
 	interface CurrentMousePos {
 		x: number;
 		y: number;
@@ -42,13 +50,32 @@ function App() {
 		console.log(currentMousePos);
 	}
 
+	function handleMouseMove(e: React.MouseEvent<HTMLDivElement>) {
+		console.log("is moving");
+
+		const slider = { x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY };
+		console.log(slider);
+		console.log(currentMousePos);
+		setResizeWidth((prev) => {
+			let newWidth = prev + 1;
+			return newWidth;
+		});
+	}
+
+	useEffect(() => {
+		console.log(resizeWidth);
+	}, [resizeWidth]);
+
 	return (
 		<div className="App">
-			<div className="editor-container">
+			<div
+				className="editor-container"
+				// style={{ width: `${resizeWidth}px` }}
+			>
 				<CodeMirror
 					value={userInput}
 					onChange={onChange}
-					maxWidth="100%"
+					// width="100%"
 					minHeight="100vh"
 					extensions={[
 						vim(),
@@ -67,33 +94,36 @@ function App() {
 						console.log(getStatistics(viewUp).lineCount);
 					}}
 				/>
+
+				<div
+					onMouseDown={handleWidthResize}
+					onMouseMove={handleMouseMove}
+					className="sheet-slider"
+				/>
 			</div>
-			<div className="markdown-wrapper">
-				<div onClick={handleWidthResize} className="sheet-slider"></div>
-				<div style={{ width: resizeWidth }} className="sheet markdown-body">
-					<ReactMarkdown
-						children={userInput}
-						remarkPlugins={[remarkGfm]}
-						components={{
-							code({ node, inline, className, children, ...props }) {
-								const match = /language-(\w+)/.exec(className || "");
-								return !inline && match ? (
-									<SyntaxHighlighter
-										{...props}
-										children={String(children).replace(/\n$/, "")}
-										style={dark}
-										language={match[1]}
-										PreTag="div"
-									/>
-								) : (
-									<code {...props} className={className}>
-										{children}
-									</code>
-								);
-							},
-						}}
-					/>
-				</div>
+			<div className="md markdown-body">
+				<ReactMarkdown
+					children={userInput}
+					remarkPlugins={[remarkGfm]}
+					components={{
+						code({ node, inline, className, children, ...props }) {
+							const match = /language-(\w+)/.exec(className || "");
+							return !inline && match ? (
+								<SyntaxHighlighter
+									{...props}
+									children={String(children).replace(/\n$/, "")}
+									style={dark}
+									language={match[1]}
+									PreTag="div"
+								/>
+							) : (
+								<code {...props} className={className}>
+									{children}
+								</code>
+							);
+						},
+					}}
+				/>
 			</div>
 		</div>
 	);
